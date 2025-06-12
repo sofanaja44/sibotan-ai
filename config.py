06672636263c5876791611eff39da9256d52a1d1
sofanaja44 <yaradit74@gmail.com>
2025-06-12 12:46:24 +0900

    🔥 Remove sensitive files from history

--- config.py
+++
@@ -1,7 +0,0 @@
-
-# config.py
-OPENAI_API_KEY = "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"
-
-# Login TradingView
-TV_USER = "sopeng2005@gmail.com"
-TV_PASS = "25Agustus2005@"
